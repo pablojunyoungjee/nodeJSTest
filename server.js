d60924529746736944fe746a01b441b4ee23ade9
JunYoungJee <pablo.jee@kakaoenterprise.com>
2020-08-28 02:14:30 +0900

    nodeJS 관련 npm, 가라DB등 기본사항 커밋

---
+++ server.js
@@ -0,0 +1,22 @@
+//sample : https://min9nim.github.io/2018/10/json-server/
+//https://medium.com/codingthesmartway-com-blog/create-a-rest-api-with-json-server-36da8680136d
+
+const jsonServer = require('json-server')
+const server = jsonServer.create()
+const path = require('path')
+const router = jsonServer.router(path.join(__dirname, 'db.json'))
+const middlewares = jsonServer.defaults();
+
+// Set default middlewares (logger, static, cors and no-cache)
+server.use(middlewares)
+
+// To handle POST, PUT and PATCH you need to use a body-parser
+// You can use the one used by JSON Server
+server.use(jsonServer.bodyParser)
+
+server.use(router)
+
+let port = 80;
+server.listen(port, () => {
+  console.log(`JSON Server is running, port(${port})`)
+})
